0b603e8da9da45fefe8df7aa0fffaa516df90f48
sterdsterd <sterdsterd@icloud.com>
2026-05-04 01:45:09 +0900

    refactor: enhance Timer class with [[nodiscard]] attributes and improve callback parameter naming

--- include/rvc/timer.hpp
+++ include/rvc/timer.hpp
@@ -15,19 +15,19 @@ public:
     void stop();
     void reset();
 
-    bool isExpired() const;
-    bool isRunning() const;
+    [[nodiscard]] bool isExpired() const;
+    [[nodiscard]] bool isRunning() const;
 
     void setDuration(std::int64_t durationMs);
-    void setCallback(Callback cb);
+    void setCallback(Callback callback);
 
     void update();
 
 private:
     static std::int64_t defaultClock();
 
-    ClockFn clock_{};
-    Callback callback_{};
+    ClockFn clock_;
+    Callback callback_;
 
     std::int64_t duration_{0};
     std::int64_t startTime_{0};
